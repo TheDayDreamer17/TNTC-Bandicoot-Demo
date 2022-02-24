b5c348dc0ba88c455ac56c6168642990bcd3f568
Vismay thaker <vismaythaker008@gmail.com>
2022-02-25 01:23:13 +0530

    Update to pepse

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Builds.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/builds for pepsi.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
